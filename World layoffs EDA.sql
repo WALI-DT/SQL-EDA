7b181684fe2bf253db48049ab45ad53923749b7e
Abubakar Abdul-Wali <abubakarabdulwali7@gmail.com>
2024-12-21 15:24:23 +0100

    code base

---
+++ World layoffs EDA.sql
@@ -0,0 +1,132 @@
+-- EXPLORATORY DATA ANALYSIS WITH THE WORLD LAYOFFS DATA --
+
+
+SELECT * 
+FROM world_layoffs.layoffs_staging2;
+
+SELECT MAX(total_laid_off)
+FROM world_layoffs.layoffs_staging2;
+
+
+
+-- PERCENTAGE LAYOFFS --
+SELECT MAX(percentage_laid_off),  MIN(percentage_laid_off)
+FROM world_layoffs.layoffs_staging2
+WHERE  percentage_laid_off IS NOT NULL;
+
+
+
+-- COMPANIES WITH 100% LAYOFFS --
+SELECT *
+FROM world_layoffs.layoffs_staging2
+WHERE  percentage_laid_off = 1;
+
+
+
+-- SIZE OF COMPANIES --
+SELECT *
+FROM world_layoffs.layoffs_staging2
+WHERE  percentage_laid_off = 1
+ORDER BY funds_raised_millions DESC;
+
+
+
+-- BIGGEST LAYOFF AT A GO --
+SELECT company, total_laid_off
+FROM world_layoffs.layoffs_staging
+ORDER BY 2 DESC
+LIMIT 5;
+
+
+
+-- BIGGEST TOTAL LAYOFFS --
+SELECT company, SUM(total_laid_off)
+FROM world_layoffs.layoffs_staging2
+GROUP BY company
+ORDER BY 2 DESC
+LIMIT 10;
+
+
+
+-- GROUPING THE SUM OF TOTAL LAYOFFS BY DIFFERNT ATTRIBUTES --
+
+
+-- BY LOCATION --
+SELECT location, SUM(total_laid_off)
+FROM world_layoffs.layoffs_staging2
+GROUP BY location
+ORDER BY 2 DESC
+LIMIT 10;
+
+
+
+-- BY COUNTRY --
+SELECT country, SUM(total_laid_off)
+FROM world_layoffs.layoffs_staging2
+GROUP BY country
+ORDER BY 2 DESC;
+
+
+
+-- BY YEAR --
+SELECT YEAR(date), SUM(total_laid_off)
+FROM world_layoffs.layoffs_staging2
+GROUP BY YEAR(date)
+ORDER BY 1 ASC;
+
+
+
+-- BY INDUSTRY --
+SELECT industry, SUM(total_laid_off)
+FROM world_layoffs.layoffs_staging2
+GROUP BY industry
+ORDER BY 2 DESC;
+
+
+
+-- STAGE i.e series a,b.. --
+SELECT stage, SUM(total_laid_off)
+FROM world_layoffs.layoffs_staging2
+GROUP BY stage
+ORDER BY 2 DESC;
+
+
+
+-- MOST LAYOFFS PER YEAR --
+WITH Company_Year AS 
+(
+  SELECT company, YEAR(date) AS years, SUM(total_laid_off) AS total_laid_off
+  FROM layoffs_staging2
+  GROUP BY company, YEAR(date)
+)
+, Company_Year_Rank AS (
+  SELECT company, years, total_laid_off, DENSE_RANK() OVER (PARTITION BY years ORDER BY total_laid_off DESC) AS ranking
+  FROM Company_Year
+)
+SELECT company, years, total_laid_off, ranking
+FROM Company_Year_Rank
+WHERE ranking <= 3
+AND years IS NOT NULL
+ORDER BY years ASC, total_laid_off DESC;
+
+
+
+-- ROLLING TOTAL OF LAYOFFS PER MONTH --
+SELECT SUBSTRING(date,1,7) as dates, SUM(total_laid_off) AS total_laid_off
+FROM layoffs_staging2
+GROUP BY dates
+ORDER BY dates ASC;
+
+
+
+-- ROLLING TOTAL AS CTE --
+WITH DATE_CTE AS 
+(
+SELECT SUBSTRING(date,1,7) as dates, SUM(total_laid_off) AS total_laid_off
+FROM layoffs_staging2
+GROUP BY dates
+ORDER BY dates ASC
+)
+SELECT dates, SUM(total_laid_off) OVER (ORDER BY dates ASC) as rolling_total_layoffs
+FROM DATE_CTE
+ORDER BY dates ASC;
